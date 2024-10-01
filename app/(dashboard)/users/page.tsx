1de802438f77dbeb17e1426e059b050ccd257da3
Beaskyy <afeez.akintobi@yahoo.com>
2024-10-01 17:09:55 +0100

    profile

--- app/(dashboard)/users/page.tsx
+++ app/(dashboard)/users/page.tsx
@@ -6,7 +6,7 @@ import { users } from "@/lib/data";
 
 const UsersPage = () => {
   return (
-    <main className="p-12">
+    <main className="lg:p-12 md:p-6 p-4">
       <h3 className="text-2xl font-medium text-[#213F7D]">Users</h3>
       <Overview />
       <DataTable
